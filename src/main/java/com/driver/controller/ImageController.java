477354061c7081e695b306edcd50b4e7741e402f
Fiona-alice <alicce.fiona@gmail.com>
2023-01-14 20:15:41 +0300

    Done

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -1,6 +1,9 @@
 package com.driver.controller;
 
+import com.driver.models.Blog;
 import com.driver.models.Image;
+import com.driver.services.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,17 +12,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/images")
 public class ImageController {
 
+    @Autowired
+    ImageService imageService;
+
     @PostMapping("/create")
     public ResponseEntity<Image> createAndReturn(@RequestBody Blog blog,
                                                  @RequestParam String description,
                                                  @RequestParam String dimensions) {
-        Image image = null;
+        Image image = imageService.createAndReturn(blog,description,dimensions);
         return new ResponseEntity<>(image, HttpStatus.CREATED);
     }
 
     @GetMapping("/countImagesInScreen/{id}/{screenDimensions}")
     public ResponseEntity<Integer> countImagesInScreen(@PathVariable int id, @PathVariable String screenDimensions){
-        int count = 0;
+        Image image=imageService.findById(id);
+        int count = imageService.countImagesInScreen(image,screenDimensions);
         return new ResponseEntity<>(count, HttpStatus.OK);
     }
 
